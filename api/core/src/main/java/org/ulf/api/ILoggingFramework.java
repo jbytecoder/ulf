519a4139846d7656d647ecbe566c2a42bdfef26f
hudziak <hudziak@cogisoft.pl>
2018-08-12 15:30:57 +0000

    Initial implementation for LoggingFramework

---
+++ api/core/src/main/java/org/ulf/api/ILoggingFramework.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ulf.api;
+
+import java.util.Map;
+
+/**
+ * Represents the desiered logging framework.
+ * 
+ * <p>
+ * This inerface abstracts away the underlying loggin framework that might
+ * be used in project. The idea is to provide as simple and rather static
+ * interface as possible to allow easy implemetations for popular 
+ * logging frameworkss
+ * <p>
+ * 
+ * 
+ * 
+ * @author jbytecoder
+ */
+public interface ILoggingFramework {
+    IMarker createMarker( Object[] data );
+    IMessage createMessage( Object[] data );
+    /**
+     * Thorws the message into the loggin framework.
+     * 
+     * This causes the message to appear in apropirate log as configured in 
+     * uderlying framework
+     * 
+     * @param message 
+     */
+    void deliver( IMessage message );
+}
--- api/core/src/main/java/org/ulf/api/LoggingFramework.java
+++ api/core/src/main/java/org/ulf/api/LoggingFramework.java
@@ -1,5 +1,9 @@
 package org.ulf.api;
 
+import java.util.Optional;
+import java.util.ServiceLoader;
+import java.util.stream.StreamSupport;
+
 /**
  * 
  * Facade over logging framework interface.
@@ -12,13 +16,52 @@ package org.ulf.api;
  * @author jbytecoder
  */
 public class LoggingFramework {
-   public static IMarker createMarker() {
-     return null;
+   private static final ILoggingFramework framework;
+   
+   static {
+     Optional<ILoggingFramework> candidate = 
+             StreamSupport.stream(
+                     ServiceLoader.load(ILoggingFramework.class)
+                                  .spliterator(),
+                     false).findFirst();
+     if( candidate.isPresent() ) {
+        framework = candidate.get();
+     } else {
+       // todo: provide a default
+       framework = null;
+     }
    }
-   public static IMessage createMessage() {
-     return null;
+  
+   /**
+    * Create a new marker;
+    * 
+    * This function delegates to {@link ILoggingFramework#createMarker(java.lang.Object[]) }
+    * 
+    * @param data
+    * @return 
+    */
+   public static IMarker createMarker(Object[] data) {
+     return framework.createMarker(data);
    }
+   /**
+    * Create a new message;
+    * 
+    * This function delegates to {@link ILoggingFramework#createMessage(java.lang.Object[]) }
+    * 
+    * @param data
+    * @return 
+    */
+   public static IMessage createMessage(Object[] data) {
+     return framework.createMessage(data);
+   }
+   /**
+    * Sends the message into the logging framework.
+    * 
+    * This function delegates to {@link ILoggingFramework#deliver(org.ulf.api.IMessage)}
+    * 
+    * @param message 
+    */
    public static void deliver( IMessage message ) {
-     
+     framework.deliver(message);
    } 
 }
