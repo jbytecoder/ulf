7ef7e04cb9d2193affa6af4063dfdf33f9f47be5
hudziak <hudziak@cogisoft.pl>
2018-08-12 16:19:09 +0000

    Initial direct api

--- api/direct/src/main/java/org/ulf/api/direct/L.java
+++ api/direct/src/main/java/org/ulf/api/direct/L.java
@@ -5,16 +5,82 @@
  */
 package org.ulf.api.direct;
 
+import org.ulf.api.IMarker;
 import org.ulf.api.IMessage;
 import org.ulf.api.LoggingFramework;
 
 /**
- *  Utility class shortenning access to {@link LoggingFramework}.
+ *  Utility class shortening access to {@link LoggingFramework}.
+ *  
+ * This class provides shortrands for creating message and loggin
  * 
  * @author jbc
  */
 public class L {
+  public static final int LEVEL_ERROR = 100;
+  public static final int LEVEL_WARNING = 200;
+  public static final int LEVEL_INFO = 300;
+  public static final int LEVEL_DEBUG = 400;
+  public static final int LEVEL_VERBOSE = 500;
+  
   public static void l( IMessage message ) {
     LoggingFramework.deliver(message);
+  }
+  public static IMessage m() {
+    return LoggingFramework.createMessage(null);
+  }
+  public static IMarker marker( String name ) {
+    return LoggingFramework.createMarker(new Object[]{name});
+  }
+  
+  public static void e( Throwable th ) {
+    l(m().at(LEVEL_ERROR).about(th));
+  }
+  public static void e( Throwable th, String message, Object... args ) {
+    l(m().at(LEVEL_ERROR).about(th).say(message, args));
+  }
+  public static void e( Class c, String method, String message, Object... args ) {
+    l(m().at(LEVEL_ERROR).from(c,method).say(message, args));
+  }
+  
+  
+  public static void w( Throwable th ) {
+    l(m().at(LEVEL_WARNING).about(th));
+  }
+  public static void w( Throwable th, String message, Object... args ) {
+    l(m().at(LEVEL_WARNING).about(th).say(message, args));
+  }
+  public static void w( Class c, String method, String message, Object... args ) {
+    l(m().at(LEVEL_WARNING).from(c,method).say(message, args));
+  }
+  
+  public static void i( Throwable th ) {
+    l(m().at(LEVEL_INFO).about(th));
+  }
+  public static void i( Throwable th, String message, Object... args ) {
+    l(m().at(LEVEL_INFO).about(th).say(message, args));
+  }
+  public static void i( Class c, String method, String message, Object... args ) {
+    l(m().at(LEVEL_INFO).from(c,method).say(message, args));
+  }
+  
+  public static void d( Throwable th ) {
+    l(m().at(LEVEL_DEBUG).about(th));
+  }
+  public static void d( Throwable th, String message, Object... args ) {
+    l(m().at(LEVEL_DEBUG).about(th).say(message, args));
+  }
+  public static void d( Class c, String method, String message, Object... args ) {
+    l(m().at(LEVEL_DEBUG).from(c,method).say(message, args));
+  }
+  
+  public static void v( Throwable th ) {
+    l(m().at(LEVEL_VERBOSE).about(th));
+  }
+  public static void v( Throwable th, String message, Object... args ) {
+    l(m().at(LEVEL_VERBOSE).about(th).say(message, args));
+  }
+  public static void v( Class c, String method, String message, Object... args ) {
+    l(m().at(LEVEL_VERBOSE).from(c,method).say(message, args));
   }
 }
--- api/direct/src/main/java/org/ulf/api/direct/M.java
+++ api/direct/src/main/java/org/ulf/api/direct/M.java
@@ -8,10 +8,42 @@ package org.ulf.api.direct;
 import org.ulf.api.IMessage;
 
 /**
- * Utility class for shortrand {@link IMessage} creation.
- * 
+ *
  * @author jbytecoder
  */
 public class M {
-  
+    public static IMessage me( Class c, String method ) {
+      return L.m().at(L.LEVEL_ERROR).from(c, method);
+    }
+    public static IMessage me( Throwable th ) {
+      return L.m().at(L.LEVEL_ERROR).about(th);
+    }
+    
+    public static IMessage mw( Class c, String method ) {
+      return L.m().at(L.LEVEL_WARNING).from(c, method);
+    }
+    public static IMessage mw( Throwable th ) {
+      return L.m().at(L.LEVEL_WARNING).about(th);
+    }
+    
+    public static IMessage mi( Class c, String method ) {
+      return L.m().at(L.LEVEL_INFO).from(c, method);
+    }
+    public static IMessage mi( Throwable th ) {
+      return L.m().at(L.LEVEL_INFO).about(th);
+    }
+    
+    public static IMessage md( Class c, String method ) {
+      return L.m().at(L.LEVEL_DEBUG).from(c, method);
+    }
+    public static IMessage md( Throwable th ) {
+      return L.m().at(L.LEVEL_DEBUG).about(th);
+    }
+    
+    public static IMessage mv( Class c, String method ) {
+      return L.m().at(L.LEVEL_VERBOSE).from(c, method);
+    }
+    public static IMessage mv( Throwable th ) {
+      return L.m().at(L.LEVEL_VERBOSE).about(th);
+    }
 }
